0b24afc669f2d93a8b03022978fa73d3fbf5570b
TaylorS15 <taylorsvec978@yahoo.com>
2022-11-29 15:51:26 -0500

    removed App and Hello. created Card skeleton

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello'
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>
+    <Card />
 );
 
 // If you want to start measuring performance in your app, pass a function
